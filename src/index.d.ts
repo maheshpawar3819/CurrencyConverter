71ea45a09be960bc019ebb8d11734a16fb8884b5
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-09-18 15:05:14 +0530

    add interface for currency

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+//create interface this will help us not deviate to much just follow this properties must have in that
+
+interface Currency {
+  name: string;
+  value: number;
+  flag: string;
+  symbol: string;
+}
