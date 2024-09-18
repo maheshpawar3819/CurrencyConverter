3db2d27973b2a47ef6645ae0407d4d96c35fcd47
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-09-18 16:26:42 +0530

    add snackbar and other logic to convert currency

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -9,14 +9,106 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+//import constants
+import {currencyByRupee} from './constants';
+import CurrencyButton from './Components/CurrencyButton';
+import Snackbar from 'react-native-snackbar';
 
 function App(): React.JSX.Element {
+  const [inputValue, setInputValue] = useState('');
+  const [resultValue, setResultValue] = useState('');
+  const [targetCurrency, setTargetCurrency] = useState('');
+
+  const buttonPressed = (targetValue: Currency) => {
+    //handle empty case
+    if (!targetValue) {
+      return Snackbar.show({
+        text: 'Plese Enter the value to convert',
+        backgroundColor: '#EA7773',
+        textColor: '#000000',
+      });
+    }
+    const inputAmount = parseFloat(inputValue);
+    //handle error case
+    if (!isNaN(inputAmount)) {
+      const convertableValue = inputAmount * targetValue.value;
+      const result = `${targetValue.symbol} ${convertableValue.toFixed(2)}`;
+      setResultValue(result);
+      setTargetCurrency(targetValue.name);
+    } else {
+      return Snackbar.show({
+        text: 'Not a valid number to convert',
+        backgroundColor: '#F4BE2C',
+        textColor: '#000000',
+      });
+    }
+  };
+
   return (
-    <>
+    <SafeAreaView>
       <View>
         <Text>hii mahi</Text>
       </View>
-    </>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#515151',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  resultTxt: {
+    fontSize: 32,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupee: {
+    marginRight: 8,
+
+    fontSize: 22,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupeesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputAmountField: {
+    height: 40,
+    width: 200,
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 4,
+    backgroundColor: '#FFFFFF',
+  },
+  bottomContainer: {
+    flex: 3,
+  },
+  button: {
+    flex: 1,
+
+    margin: 12,
+    height: 60,
+
+    borderRadius: 12,
+    backgroundColor: '#fff',
+    elevation: 2,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+  },
+  selected: {
+    backgroundColor: '#ffeaa7',
+  },
+});
 export default App;
